92e9a98fefc6f022d6eab7f3cabeb04b895750ad
Ryan Gard <ryan.gard@rackspace.com>
2018-08-08 14:53:40 -0700

    ASC-441 Add Test Case Rename/Move Integration Tests
    Add integration tests that cover expected behavior when an automation engineer
    renames or moves a test case within a given Git repo under test.
    
    Also, I discovered more bugs and small typos that are fixed in this commit.

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -5,6 +5,7 @@
 # ======================================================================================================================
 # Imports
 # ======================================================================================================================
+import uuid
 import pytest
 
 
@@ -243,3 +244,167 @@ class TestMixed(object):
             test_runs = test.qtest_test_runs
             assert len(test_runs) == 1
             pytest.helpers.assert_qtest_property(test_runs[0], 'Status', test_run_status_exp)
+
+
+class TestRename(object):
+    """Validate that test cases can be renamed."""
+
+    # noinspection PyUnresolvedReferences
+    def test_rename_test_case_for_asc(self, single_passing_test_for_asc):
+        """Verify that renaming a test case (keeping the same 'test_id') preserves the original test case name, but
+        the associated test run uses the updated name for the "asc" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_asc.assert_invoke_zigzag()
+
+        # Expectations
+        original_test_case_name_exp = single_passing_test_for_asc.tests[0].name
+        new_test_case_name_exp = "test_rename_{}".format(str(uuid.uuid1()))
+
+        # Test
+        single_passing_test_for_asc.tests[0].name = new_test_case_name_exp
+        single_passing_test_for_asc.assert_invoke_zigzag(force_clean_up=False)
+
+        pytest.helpers.assert_qtest_property(single_passing_test_for_asc.tests[0].qtest_test_case_info,
+                                             'name',
+                                             original_test_case_name_exp)
+
+        pytest.helpers.assert_qtest_property(single_passing_test_for_asc.tests[0].qtest_test_runs[0],
+                                             'name',
+                                             new_test_case_name_exp)
+
+    # noinspection PyUnresolvedReferences
+    def test_rename_test_case_for_mk8s(self, single_passing_test_for_mk8s):
+        """Verify that renaming a test case (keeping the same 'test_id') preserves the original test case name, but
+        the associated test run uses the updated name for the "mk8s" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_mk8s.assert_invoke_zigzag()
+
+        # Expectations
+        original_test_case_name_exp = single_passing_test_for_mk8s.tests[0].name
+        new_test_case_name_exp = "test_rename_{}".format(str(uuid.uuid1()))
+
+        # Test
+        single_passing_test_for_mk8s.tests[0].name = new_test_case_name_exp
+        single_passing_test_for_mk8s.assert_invoke_zigzag(force_clean_up=False)
+
+        pytest.helpers.assert_qtest_property(single_passing_test_for_mk8s.tests[0].qtest_test_case_info,
+                                             'name',
+                                             original_test_case_name_exp)
+
+        pytest.helpers.assert_qtest_property(single_passing_test_for_mk8s.tests[0].qtest_test_runs[0],
+                                             'name',
+                                             new_test_case_name_exp)
+
+    # noinspection PyUnresolvedReferences
+    def test_move_test_case_to_suite_for_asc(self, single_passing_test_for_asc):
+        """Verify that moving a test case to a test suite (keeping the same 'test_id') will show execution results under
+        the old test cycle and under a new test cycle named after the suite for the "asc" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_asc.assert_invoke_zigzag()
+        original_parent_test_cycle = single_passing_test_for_asc.tests[0].qtest_parent_test_cycles[0].name
+
+        # Expectations
+        new_parent_test_cycle_name_exp = 'TestSuite'
+
+        # Test
+        single_passing_test_for_asc.tests[0].class_name = "tests.test_default.{}".format(new_parent_test_cycle_name_exp)
+        single_passing_test_for_asc.assert_invoke_zigzag(force_clean_up=False)
+        parent_test_cycles = single_passing_test_for_asc.tests[0].qtest_parent_test_cycles
+
+        # The qTest API returns test associated test runs from newest to oldest.
+        pytest.helpers.assert_qtest_property(parent_test_cycles[0],
+                                             'name',
+                                             new_parent_test_cycle_name_exp)
+
+        pytest.helpers.assert_qtest_property(parent_test_cycles[1],
+                                             'name',
+                                             original_parent_test_cycle)
+
+    # noinspection PyUnresolvedReferences
+    def test_move_test_case_to_suite_for_mk8s(self, single_passing_test_for_mk8s):
+        """Verify that moving a test case to a test suite (keeping the same 'test_id') will show execution results under
+        the old test cycle and under a new test cycle named after the suite for the "asc" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_mk8s.assert_invoke_zigzag()
+        original_parent_test_cycle = single_passing_test_for_mk8s.tests[0].qtest_parent_test_cycles[0].name
+
+        # Expectations
+        new_parent_test_cycle_name_exp = 'tests.test_default.TestSuite'
+
+        # Test
+        single_passing_test_for_mk8s.tests[0].class_name = new_parent_test_cycle_name_exp
+        single_passing_test_for_mk8s.assert_invoke_zigzag(force_clean_up=False)
+        parent_test_cycles = single_passing_test_for_mk8s.tests[0].qtest_parent_test_cycles
+
+        # The qTest API returns test associated test runs from newest to oldest.
+        pytest.helpers.assert_qtest_property(parent_test_cycles[0],
+                                             'name',
+                                             new_parent_test_cycle_name_exp)
+
+        pytest.helpers.assert_qtest_property(parent_test_cycles[1],
+                                             'name',
+                                             original_parent_test_cycle)
+
+    # noinspection PyUnresolvedReferences
+    def test_move_test_case_to_different_file_for_asc(self, single_passing_test_for_asc):
+        """Verify that moving a test case to a test suite (keeping the same 'test_id') will show execution results under
+        the old test cycle and under a new test cycle named after the the file name for the "mk8s" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_asc.assert_invoke_zigzag()
+        original_parent_test_cycle = single_passing_test_for_asc.tests[0].qtest_parent_test_cycles[0].name
+
+        # Expectations
+        new_parent_test_cycle_name_exp = 'test_fancy'
+
+        # Test
+        single_passing_test_for_asc.tests[0].class_name = "tests.{}".format(new_parent_test_cycle_name_exp)
+        single_passing_test_for_asc.tests[0].file_path = "tests/{}.py".format(new_parent_test_cycle_name_exp)
+        single_passing_test_for_asc.assert_invoke_zigzag(force_clean_up=False)
+        parent_test_cycles = single_passing_test_for_asc.tests[0].qtest_parent_test_cycles
+
+        # The qTest API returns test associated test runs from newest to oldest.
+        pytest.helpers.assert_qtest_property(parent_test_cycles[0],
+                                             'name',
+                                             new_parent_test_cycle_name_exp)
+
+        pytest.helpers.assert_qtest_property(parent_test_cycles[1],
+                                             'name',
+                                             original_parent_test_cycle)
+
+    # noinspection PyUnresolvedReferences
+    def test_move_test_case_to_different_file_for_mk8s(self, single_passing_test_for_mk8s):
+        """Verify that moving a test case to a test suite (keeping the same 'test_id') will show execution results under
+        the old test cycle and under a new test cycle named after the the file name for the "mk8s" CI environment.
+        """
+
+        # Setup
+        single_passing_test_for_mk8s.assert_invoke_zigzag()
+        original_parent_test_cycle = single_passing_test_for_mk8s.tests[0].qtest_parent_test_cycles[0].name
+
+        # Expectations
+        new_parent_test_cycle_name_exp = 'tests.test_fancy'
+
+        # Test
+        single_passing_test_for_mk8s.tests[0].class_name = new_parent_test_cycle_name_exp
+        single_passing_test_for_mk8s.tests[0].file_path = 'tests/test_fancy.py'
+        single_passing_test_for_mk8s.assert_invoke_zigzag(force_clean_up=False)
+        parent_test_cycles = single_passing_test_for_mk8s.tests[0].qtest_parent_test_cycles
+
+        # The qTest API returns test associated test runs from newest to oldest.
+        pytest.helpers.assert_qtest_property(parent_test_cycles[0],
+                                             'name',
+                                             new_parent_test_cycle_name_exp)
+
+        pytest.helpers.assert_qtest_property(parent_test_cycles[1],
+                                             'name',
+                                             original_parent_test_cycle)
