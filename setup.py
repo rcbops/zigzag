ad6fad13176bfebace5748634a003ed68f1a2fb9
butcher <mark.butcher@rackspace.com>
2018-08-14 15:42:45 -0700

    Bump version: 0.11.0 → 0.11.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ entry_points = {
 
 setup(
     name='rpc-zigzag',
-    version='0.11.0',
+    version='0.11.1',
     author="rcbops",
     author_email='rcb-deploy@lists.rackspace.com',
     maintainer='rcbops',
