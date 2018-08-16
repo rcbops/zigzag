c93b79cf8a87c520808d1d364aa1b26c4c39d7c3
Ryan Gard <ryan.gard@rackspace.com>
2018-08-16 14:23:43 -0700

    Bump version: 0.11.1 → 0.11.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ entry_points = {
 
 setup(
     name='rpc-zigzag',
-    version='0.11.1',
+    version='0.11.2',
     author="rcbops",
     author_email='rcb-deploy@lists.rackspace.com',
     maintainer='rcbops',
